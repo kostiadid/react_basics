febb41217807c161e96509f61aacf30652425bd2
kostia <kostiadid228@gmail.com>
2026-07-19 15:35:55 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My App</h1>
+        <button>Click Me</button>
+      </header>
+    </div>
+  );
+}
